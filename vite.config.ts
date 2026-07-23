2ce8e47e39f53a4b8215ab36d6477883cab82040
Eagle Won <wxy112300@163.com>
2026-07-24 03:27:44 +0800

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist/renderer",
+    emptyOutDir: true
+  },
+  server: {
+    port: 5173,
+    strictPort: true
+  }
+});
